9dc96fd9a585bf252a65b2c51a3f2aef8922e0f2
DinoCzar <ryanb_booking@yahoo.com>
2023-04-11 01:27:00 -0700

    Add module functionality and css styling

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-headerButton=document.querySelector("#header-button"),headerButton.addEventListener("click",(e=>{console.log("test")}));
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*
+
+const input = document.querySelector('input'),
+h2 = document.querySelector('h2')
+
+input.addEventListener('keyup', display);
+
+function display() {
+    localStorage.setItem('value', input.value)
+}
+
+h2.innerHTML = localStorage.getItem('value')
+
+*/
+
+headerButton = document.querySelector('#header-button');
+headerButton.addEventListener('click', (e) => {
+	addNew();
+});
+
+function addNew() {
+	modal.style.display = 'block';
+	overlay.style.display = 'block';
+}
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBOztBQUVBO0FBQ0E7O0FBRUE7O0FBRUE7QUFDQTtBQUNBOztBQUVBOztBQUVBOztBQUVBO0FBQ0E7QUFDQTtBQUNBLENBQUM7O0FBRUQ7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90by1kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8qXG5cbmNvbnN0IGlucHV0ID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignaW5wdXQnKSxcbmgyID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcignaDInKVxuXG5pbnB1dC5hZGRFdmVudExpc3RlbmVyKCdrZXl1cCcsIGRpc3BsYXkpO1xuXG5mdW5jdGlvbiBkaXNwbGF5KCkge1xuICAgIGxvY2FsU3RvcmFnZS5zZXRJdGVtKCd2YWx1ZScsIGlucHV0LnZhbHVlKVxufVxuXG5oMi5pbm5lckhUTUwgPSBsb2NhbFN0b3JhZ2UuZ2V0SXRlbSgndmFsdWUnKVxuXG4qL1xuXG5oZWFkZXJCdXR0b24gPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcjaGVhZGVyLWJ1dHRvbicpO1xuaGVhZGVyQnV0dG9uLmFkZEV2ZW50TGlzdGVuZXIoJ2NsaWNrJywgKGUpID0+IHtcblx0YWRkTmV3KCk7XG59KTtcblxuZnVuY3Rpb24gYWRkTmV3KCkge1xuXHRtb2RhbC5zdHlsZS5kaXNwbGF5ID0gJ2Jsb2NrJztcblx0b3ZlcmxheS5zdHlsZS5kaXNwbGF5ID0gJ2Jsb2NrJztcbn1cbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
