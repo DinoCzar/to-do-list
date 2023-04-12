e14b02745edd0605e9d1c27c1d778c43b71471c3
DinoCzar <ryanb_booking@yahoo.com>
2023-04-12 00:28:53 -0700

    Add edit and notes buttons for tasks

--- src/task.js
+++ src/task.js
@@ -12,13 +12,26 @@ function createTask(task) {
 
 	const due = document.createElement('div');
 	due.classList.add('task-due');
-	due.textContent = task.due;
+	due.textContent = 'Due: ' + task.due;
 	taskDiv.appendChild(due);
 
 	const notes = document.createElement('div');
 	notes.classList.add('task-notes');
-	notes.textContent = task.notes;
+	notes.textContent = 'Notes: ' + task.notes;
 	taskDiv.appendChild(notes);
+
+    const editTask = document.createElement('button');
+	editTask.classList.add('edit-task');
+	editTask.textContent = 'edit';
+	taskDiv.appendChild(editTask);
+
+    const viewNotes = document.createElement('button');
+	viewNotes.classList.add('view-notes');
+	viewNotes.textContent = '...';
+	taskDiv.appendChild(viewNotes);
+    viewNotes.addEventListener('click', (e) => {
+        notes.style.display = 'block';
+    });
 }
 
 export default createTask;
