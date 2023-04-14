fb54a17eb81fe729c4b4b299bf6b18d5948b0818
DinoCzar <ryanb_booking@yahoo.com>
2023-04-14 16:05:26 -0700

    Remove load-edit-modal

--- src/edit-task.js
+++ src/edit-task.js
@@ -1,7 +1,47 @@
-import loadEditModal from './load-edit-modal';
-
 function editTask(task, taskId) {
-	loadEditModal(task, taskId);
+	displayEditValues(task);
+	displayEditModal();
+
+	const cancelEditButton = document.querySelector('#cancel-edit-button');
+	cancelEditButton.addEventListener('click', (e) => {
+		clearEditForms();
+		hideEditModal();
+	});
+}
+
+const editButton = document.querySelector('#edit-button');
+editButton.addEventListener('click', (e) => {
+	console.log('edit item');
+	clearEditForms();
+	hideEditModal();
+});
+
+const editModal = document.querySelector('#edit-modal');
+
+const editTitle = document.querySelector('#edit-title');
+const editDue = document.querySelector('#edit-due');
+const editNotes = document.querySelector('#edit-notes');
+
+function displayEditValues(task) {
+	editTitle.value = task.title;
+	editDue.value = task.due;
+	editNotes.value = task.notes;
+}
+
+function displayEditModal() {
+	editModal.style.display = 'block';
+	overlay.style.display = 'block';
+}
+
+function hideEditModal() {
+	editModal.style.display = 'none';
+	overlay.style.display = 'none';
+}
+
+function clearEditForms() {
+	editTitle.value = null;
+	editDue.value = null;
+	editNotes.value = null;
 }
 
 export default editTask;
